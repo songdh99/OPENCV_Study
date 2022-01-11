22b761df927fe0322e9279d8bec02dc42b9f332c
Heon <songdh99@gmail.com>
2022-01-11 10:38:22 +0900

    first class

---
+++ build/CMakeFiles/testcv.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/honey/workspace/cvnew/main.cpp" "/home/honey/workspace/cvnew/build/CMakeFiles/testcv.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/honey/mylib/lib/opencv-4.5.4/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/testcv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testcv.dir/main.cpp.o"
+  "testcv"
+  "testcv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testcv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
